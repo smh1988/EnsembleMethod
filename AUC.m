cc369e571b00da591d0ee184aa2e09fcbc92c892
mahdi <smh1988@gmail.com>
2017-11-02 13:59:08 +0330

    adding empty AUC to test matlab commiting without pass

---
+++ AUC.m
@@ -0,0 +1,10 @@
+function [ output_args ] = AUC( input_args )
+%AUC calculating the area under the ROC curve
+%   based on 2012-A Quantitative Evaluation of Confidence Measures for
+%   Stereo Vision (section 4.1)
+
+
+
+
+end
+
