3f40ae99404ecacb320b40e1627c610c442f3d21
mahdi <smh1988@gmail.com>
2017-11-05 13:16:32 +0330

    a little bit faster

--- Algorithms/NCC/NCC.m
+++ Algorithms/NCC/NCC.m
@@ -7,11 +7,11 @@ maxdisp=DisparityRange(2);
 w=5;
 halfW=(w-1)/2;
 
-imgL=rgb2gray(imgL);%for now they are not seperated
-imgR=rgb2gray(imgR);
+imgL=double(rgb2gray(imgL))/255;%for now they are not seperated
+imgR=double(rgb2gray(imgR))/255;
 
-imgL=imgL';
-imgR=imgR';
+imgL=permute(imgL,[2 1 3]);
+imgR=permute(imgR,[2 1 3]);
 % rtmp=r;
 % r=l';%for right to left disparity
 % l=rtmp';%for right to left disparity
@@ -26,85 +26,86 @@ meansL=imfilter(imgL, h);
 meansR=imfilter(imgR, h);
 stdsL=stdfilt(imgL,ones(w,w));
 stdsR=stdfilt(imgR,ones(w,w));
-imgL_d=zeros(size(imgL));
+%imgL_d=zeros(size(imgL));
+CostVol = zeros (size(imgL,1),size(imgL,2),maxdisp);
+%MMN=zeros(size(imgL));
 
+%TODO: using arrayfun
+costs=zeros(maxdisp,1);
 for x=1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp
     for y=1+halfW:size(imgL,2)-halfW
         roiL=imgL((x-halfW):(x+halfW),(y-halfW):(y+halfW));
         meanL=meansL(x,y);
         stdL=stdsL(x,y);
-        
         for d=1:maxdisp
             %Cost computing ->NCC
             sum1=0;
-            
             roiR=imgR((x-halfW-d):(x+halfW-d),(y-halfW):(y+halfW));%type casting!
             %roiR=double(r((x-halfW+d):(x+halfW+d),(y-halfW):(y+halfW)));%for right to left disparity
             meanR=meansR(x-d,y);
             stdR=stdsR(x-d,y);
             for i=1:w
-                for j=1:w %notation of the paper was wrong!
-                    sum1=sum1-(roiL(i,j)-meanL)*(roiR(i,j)-meanR);
+                for j=1:w %notation of the paper was not good!
+                    sum1=sum1+(roiL(i,j)-meanL)*(roiR(i,j)-meanR);
                 end
             end
-            costs(d)=sum1/(stdL*stdR);
-            if costs(d)>0
-                costs(d)=0;
-            end
+            costs(d)=-(sum1/25)/(stdL*stdR);%FIX: this is not mentioned in the paper that it should be devided by w*w, while all plots are [0 1]!
         end
-
-        %WTA
-        [val ,ind ]=min(costs);
-        imgL_d(x,y)=ind;
-        %valMap(x,y)=val;
+        %cost volume
+        CostVol(x,y,:)=costs;
     end
 end
 
-imgL_d=imcrop(imgL_d,[1+halfW 1+halfW+maxdisp imgsize(2)-1 imgsize(1)-1]);
-imgL_d=imgL_d';
+%cropping and transposing
+CostVolume=permute(conj(CostVol(1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp,1+halfW:size(imgL,2)-halfW,:)),[2 1 3]);
+CostVolume(isinf(CostVolume))=1;
+
+tmpCostVol=CostVolume;
+tmpCostVol(tmpCostVol>0)=0;%All positive cost values are truncated to 0.
+%minimum matching cost and WTA
+[ ~ ,imgL_d]=min(tmpCostVol,[],3); 
+
 
 
 
 %% dont have time o optimize
 [imgL, imgR]=deal(imgR, imgL);
 [meansL, meansR]=deal(meansR, meansL);
-[stdsL stdsR ]=deal(stdsL, stdsL);
-imgR_d=zeros(size(imgL));
+[stdsL ,stdsR ]=deal(stdsL, stdsL);
 
+costs=zeros(maxdisp,1);
 for x=1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp
     for y=1+halfW:size(imgL,2)-halfW
         roiL=imgL((x-halfW):(x+halfW),(y-halfW):(y+halfW));
         meanL=meansL(x,y);
         stdL=stdsL(x,y);
-        
         for d=1:maxdisp
             %Cost computing ->NCC
             sum1=0;
-            
             %roiR=imgR((x-halfW-d):(x+halfW-d),(y-halfW):(y+halfW));%type casting!
             roiR=imgR((x-halfW+d):(x+halfW+d),(y-halfW):(y+halfW));%for right to left disparity
             meanR=meansR(x+d,y);
             stdR=stdsR(x+d,y);
             for i=1:w
-                for j=1:w %notation of the paper was wrong!
-                    sum1=sum1-(roiL(i,j)-meanL)*(roiR(i,j)-meanR);
+                for j=1:w %notation of the paper was not good!
+                    sum1=sum1+(roiL(i,j)-meanL)*(roiR(i,j)-meanR);
                 end
             end
-            costs(d)=sum1/(stdL*stdR);
-            if costs(d)>0
-                costs(d)=0;
-            end
+            costs(d)=-(sum1/25)/(stdL*stdR);%FIX: this is not mentioned in the paper that it should be devided by w*w, while all plots are [0 1]!
         end
-
-        %WTA
-        [val ,ind ]=min(costs);
-        imgR_d(x,y)=ind;
-        %valMap(x,y)=val;
+        %cost volume
+        CostVol(x,y,:)=costs;
     end
 end
 
-imgR_d=imcrop(imgR_d,[1+halfW 1+halfW+maxdisp imgsize(2)-1 imgsize(1)-1]);
-imgR_d=imgR_d';
+%cropping and transposing
+CostVolume=permute(conj(CostVol(1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp,1+halfW:size(imgL,2)-halfW,:)),[2 1 3]);
+CostVolume(isinf(CostVolume))=1;
+
+tmpCostVol=CostVolume;
+tmpCostVol(tmpCostVol>0)=0;%All positive cost values are truncated to 0.
+%minimum matching cost and WTA
+[ ~ ,imgR_d]=min(tmpCostVol,[],3);
 
 end
 
