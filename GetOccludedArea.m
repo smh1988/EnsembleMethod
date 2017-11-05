f0d13eab5b0955e51403a670e662ec2a08090732
mahdi <smh1988@gmail.com>
2017-11-05 14:11:49 +0330

    fixing middlebury 2005 and 2006 (occluded areas are now considered)

--- GetOccludedArea.m
+++ GetOccludedArea.m
@@ -1,24 +1,23 @@
-function [ occArea ] = GetOccludedArea( imgL_d,imgR_d )
+function [ occArea ] = GetOccludedArea( imgL_d,imgR_d ) %FIX:this is just a draft crosscheck
 %GETOCCLUDEDAREA to perform crosscheking in order to get occluded areas
-%   this code is just for Middlebury 2005 and 2006
+%   this code is just for Middlebury 2005 and 2006 and left to right check
 %   disparities should be uint8
 
 imgL_d=uint8(imgL_d');
-imgR_d=uint8(imgR_d');
+%imgR_d=uint8(imgR_d');
+occArea=zeros(size(imgL_d));
 for x=1:size(imgL_d,1)
     for y=1:size(imgL_d,2)
         lDispValue=imgL_d(x,y);
         if (x-lDispValue)>=1
             %err=abs(lDispValue-imgR_d(x-lDispValue,y));
-            occArea(x,y)=1;
-        else
             occArea(x,y)=0;
+        else
+            occArea(x,y)=1;
         end
     end
 end
 
-%totalLRC=sum(sum(lrc));
-%accuracy=(totalLRC/(size(displ,1)*size(displ,2)))*100
 occArea=logical(occArea');
 end
 
