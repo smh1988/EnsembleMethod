3b66798ee3ed9cca77b1f9daa2811742658a4f82
mahdi <smh1988@gmail.com>
2017-11-12 22:38:06 +0330

    fixing error calculation for middlebury 06 05 thedenominator should be sum of imgMask

--- GetROC.m
+++ GetROC.m
@@ -1,20 +1,23 @@
 function [ ROCCurve ,pers] = GetROC( ImageStruct, disp , confidence )
 %GetROC calculating the commulative error of %5 %10 ...%100
 %   
-[ ~ , ~ , badPixels] = EvaluateDisp(ImageStruct,disp,1);
+[ ~ , imgMask , badPixels] = EvaluateDisp(ImageStruct,disp,1);
 
 total=size(disp,1)*size(disp,2);
-tmp=zeros(total,2);
+tmp=zeros(total,3);
 tmp(:,1)=reshape(badPixels,[total ,1]);
 tmp(:,2)=reshape(confidence,[total ,1]);
+tmp(:,3)=reshape(imgMask,[total ,1]);
+
 [tmp(:,2), sortIndices]=sort(tmp(:,2),'descend');
 tmp(:,1)=tmp(sortIndices,1);
+tmp(:,3)=tmp(sortIndices,3);
 
 for i=1:20
     percent=i*0.05;
     ind=int32(total*percent);
     tmpErr=sum(tmp(1: ind,1));
-    ROCCurve(i)=double(tmpErr)/double(ind); %FIX: this should be number of mask
+    ROCCurve(i)=double(tmpErr)/sum(tmp(1: ind,3));
     pers(i)=percent;
 end
 
