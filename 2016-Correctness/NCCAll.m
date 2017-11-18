be011b5700d7b8dd1bfb8fe4ba51f2d4a5a6aac5
mahdi <smh1988@gmail.com>
2017-11-18 13:49:17 +0330

    it returns costvolumes as well

--- 2016-Correctness/NCCAll.m
+++ 2016-Correctness/NCCAll.m
@@ -1,6 +1,7 @@
-function [ imgL_d, Cost, CostVolume,MMN ] = NCCAll( imgL,imgR,DisparityRange)
+function [ imgL_d, imgR_d, Cost,CostR, CostVolume,CostVolumeR] = NCCAll( imgL,imgR,DisparityRange)
 %fast NCC Normalized cross-corrilation in 5x5 window
 %   it only gives left features
+epsilon=realmin;
 
 maxdisp=DisparityRange(2);
 
@@ -27,10 +28,9 @@ meansR=imfilter(imgR, h);
 stdsL=stdfilt(imgL,ones(w,w));
 stdsR=stdfilt(imgR,ones(w,w));
 %imgL_d=zeros(size(imgL));
-CostVol = zeros (size(imgL,1),size(imgL,2),maxdisp);
-%MMN=zeros(size(imgL));
 
-%TODO: using arrayfun
+%% TODO: using arrayfun, left image
+CostVol = zeros (imgsize(1),imgsize(2),maxdisp);
 costs=zeros(maxdisp,1);
 for x=1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp
     for y=1+halfW:size(imgL,2)-halfW
@@ -56,37 +56,68 @@ for x=1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp
     end
 end
 
+
 %cropping and transposing
 CostVolume=permute(conj(CostVol(1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp,1+halfW:size(imgL,2)-halfW,:)),[2 1 3]);
 CostVolume(isinf(CostVolume))=1;
 
 tmpCostVol=CostVolume;
 tmpCostVol(tmpCostVol>0)=0;%All positive cost values are truncated to 0.
+
 %minimum matching cost and WTA
-[ Cost ,imgL_d]=min(tmpCostVol,[],3); 
-Cost=-Cost;%this could be also a confidence measure
+[ Cost ,imgL_d]=min(tmpCostVol,[],3);
+Cost=-Cost;%this could be also a confidence measure 
 
-%Maximum Margin
-sortedCostVol =sort(CostVolume,3);
-MMN=abs(sortedCostVol(:,:,1)-sortedCostVol(:,:,2));
-
-%Left–Right Difference (LRD)
+%sortedCostVol =sort(CostVolume,3);
 
 
-%Attainable Maximum Likelihood (AML)
-% likelihoodCostVol=zeros(size(CostVolume));
-% likelihood=zeros(size(CostVolume));
-% for x=1:size(CostVolume,1)
-%     for y=1:size(CostVolume,2)
-%         for d=1:maxdisp
-%             tmp=exp(-(CostVolume(x,y,:)-Cost(x,y))./( 2*(0.2^2) ) );
-%             likelihood(x,y,:)=tmp;
-%             %normalized
-%             likelihoodCostVol(x,y,:)=tmp/sum(tmp);
-%             %AMLVol(x,y,:)=;
-%         end
-%     end
-% end
+%% right image
+%dont have time to optimize
+
+CostVolR = zeros (imgsize(1),imgsize(2),maxdisp);
+%right image disparity
+[imgL, imgR]=deal(imgR, imgL);
+[meansL, meansR]=deal(meansR, meansL);
+[stdsL ,stdsR ]=deal(stdsR, stdsL);
+
+costs=zeros(maxdisp,1);
+for x=1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp
+    for y=1+halfW:size(imgL,2)-halfW
+        roiL=imgL((x-halfW):(x+halfW),(y-halfW):(y+halfW));
+        meanL=meansL(x,y);
+        stdL=stdsL(x,y);
+        for d=1:maxdisp
+            %Cost computing ->NCC
+            sum1=0;
+            %roiR=imgR((x-halfW-d):(x+halfW-d),(y-halfW):(y+halfW));%type casting!
+            roiR=imgR((x-halfW+d):(x+halfW+d),(y-halfW):(y+halfW));%for right to left disparity
+            meanR=meansR(x+d,y);
+            stdR=stdsR(x+d,y);
+            for i=1:w
+                for j=1:w %notation of the paper was not good!
+                    sum1=sum1+(roiL(i,j)-meanL)*(roiR(i,j)-meanR);
+                end
+            end
+            costs(d)=-(sum1/25)/(stdL*stdR);%FIX: this is not mentioned in the paper that it should be devided by w*w, while all plots are [0 1]!
+        end
+        %cost volume
+        CostVolR(x,y,:)=costs;
+    end
+end
+
+%cropping and transposing
+CostVolumeR=permute(conj(CostVolR(1+halfW+maxdisp:size(imgL,1)-halfW-maxdisp,1+halfW:size(imgL,2)-halfW,:)),[2 1 3]);
+CostVolumeR(isinf(CostVolumeR))=1;
+
+
+tmpCostVol=CostVolumeR;
+tmpCostVol(tmpCostVol>0)=0;%All positive cost values are truncated to 0.
+
+%minimum matching cost and WTA
+[ CostR ,imgR_d]=min(tmpCostVol,[],3);
+CostR=-CostR;
+
+%sortedCostVolR =sort(CostVolumeR,3);
 
 end
 
