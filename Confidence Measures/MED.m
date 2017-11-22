2693d98c84b814be4e86d6a04f273e457790a9a2
mahdi <smh1988@gmail.com>
2017-11-22 13:30:51 +0330

    computed another way!

--- Confidence Measures/MED.m
+++ Confidence Measures/MED.m
@@ -1,26 +1,8 @@
 function [ MEDMap ] = MED( imgDisparity )
 %MED Difference with Median Disparity
 
-%w=5;
-halfW=2;
-
-tmpDisp=padarray(imgDisparity',[halfW halfW],'replicate');
-MEDMap=zeros(size(imgDisparity,2),size(imgDisparity,1));
-
-for x=1+halfW:size(tmpDisp,1)-halfW
-    for y=1+halfW:size(tmpDisp,2)-halfW
-        center=tmpDisp(x,y);
-        roi=double(tmpDisp((x-halfW):(x+halfW),(y-halfW):(y+halfW)));
-        mean=mean2(roi);
-        diff=abs(center-mean);
-        diff=floor(diff);
-        if diff==0 || diff==1
-            MEDMap(x-halfW,y-halfW)=diff;
-        else
-            MEDMap(x-halfW,y-halfW)=2;
-        end
-    end
-end
-MEDMap=MEDMap';
+MEDMap = medfilt2(imgDisparity, [5 5]) - imgDisparity;
+MEDMap = abs(MEDMap);
+MEDMap(MEDMap>1)=2;
 end
 
