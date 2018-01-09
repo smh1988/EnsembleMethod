c448ab1a4354a6a0cf0f74ff6155b132af658bad
mahdi <smh1988@gmail.com>
2018-01-09 08:31:32 +0330

    new file added

---
+++ visualcomparison.m
@@ -0,0 +1,89 @@
+%% Initialization
+close all;
+clear all;
+clc;
+
+%loading image names and locations
+DatasetDir;
+
+%loading all functions in arrays
+FunctionsDir;
+
+algosNum = [ 4 5 9 10 11] ;                                 %<<<-----------------------HARD CODED
+%select desired algorithms from the list below and put its number in the list
+%1-ADSM  2-ARWSM 3-BMSM  4-BSM   5-ELAS  6-FCVFSM   7-SGSM  8-SSCA  9-WCSM
+%10-MeshSM 11-NCC
+
+cmNum = [ 7 9 10] ;
+%select desired Confidence Measures from the list below and put its number in the list
+%   1-AML  2-DB 3-DD 4-HGM 5-LRC 6-LRD 7-MED 8-MM
+
+% to be used in PSO Optimization
+% addpath('optimizers');
+% fun=str2func('EvaluateWeights');
+% global finalScores k imgPixelCountTest imgPixelCountTrain dispData AllImages imagesList errThreshold
+
+maxRun=1;
+treesCount=50;
+MinLS=1000;
+NumPTS=1;
+
+AgreementThreshold=1;   %to extract ai features
+ErrorThreshold=1;       %error threshold for disparity errors
+load('MiddleRes.mat');    %RF-kMs
+
+%% reading or calculating errors for images (left and right)
+display ('calculating disparities...');
+data=struct;
+dispData=struct;
+
+%real image mumbers in AllImages
+
+        trainImageList=[];                                   %<<<-----------------------HARD CODED
+        testImageList=[703];                                        %<<<-----------------------HARD CODED
+imagesList = [ trainImageList ,testImageList];
+
+for imgNum=1:size(imagesList,2) %local image numbers
+    imgL=imread(AllImages(imagesList(imgNum)).LImage);
+    imgR=imread(AllImages(imagesList(imgNum)).RImage);
+    algoCount=0;
+    for aNum=algosNum
+        algoCount=algoCount+1;
+        fileName=strcat('./Results/',num2str(imagesList(imgNum)) ,'_',AllImages(imagesList(imgNum)).ImageName , '_'  ,func2str( algoFunc{aNum}),  '.mat');
+        load(fileName);
+        %err(algoCount,imgNum)=EvaluateDisp(AllImages(imagesList(imgNum)),data.DisparityLeft,errThreshold);%data.ErrorRates;
+        dispData(algoCount,imgNum).left=data.DisparityLeft;
+        dispData(algoCount,imgNum).right=data.DisparityRight;
+    end
+    display([num2str(imagesList(imgNum)) 'done']);
+end
+imgGT = GetGT(AllImages(imagesList(imgNum)));
+imgGTr=double(imread(AllImages(imagesList(imgNum)).RDispOcc))/3; 
+occarea=GetOccludedArea(imgGT,imgGTr);
+maxdisp=85;
+[ dispError , imgMask , badPixels] = EvaluateDisp(AllImages(imagesList(imgNum)),MiddleRes(testImageList-692).FinalDisp,1);
+
+mymap = [239,71,111
+255,209,102
+6,214,160
+17,138,178
+74,80,155];
+figure;
+xr=40:140;
+yr=200:300;
+ours=MiddleRes(testImageList-692).FinalDisp(xr,yr)/maxdisp;
+subplot(5,2,2);imshow(imgGT(xr,yr)/maxdisp);title('??? ????');
+subplot(5,2,1);imshow(imgL);title('????? ??');
+subplot(5,2,3);imshow( dispData(1,imgNum).left(xr,yr)/maxdisp);   title('BSM');
+subplot(5,2,4);imshow( dispData(2,imgNum).left(xr,yr)/maxdisp);   title('ELAS');
+subplot(5,2,5);imshow( dispData(3,imgNum).left(xr,yr)/maxdisp);   title('Census');
+subplot(5,2,6);imshow( dispData(4,imgNum).left(xr,yr)/maxdisp);   title('MeshStereo');
+subplot(5,2,7);imshow( dispData(5,imgNum).left(xr,yr)/maxdisp);   title('NCC');
+subplot(5,2,8);imshow( MiddleRes(testImageList-692).FinalDisp(xr,yr)/maxdisp);   title('??? ????????');
+ax1=subplot(5,2,[9,10]);
+imshow( MiddleRes(testImageList-692).Indices(xr,yr),[]);
+title('????? ??');
+colormap(ax1,mymap/255);
+%subplot(5,2,10);imshow( badPixels(xr,yr));   title('???? ???');
+%subplot(5,2,10);imshow(occarea(xr,yr));title('????? ??????');
+tightfig;
